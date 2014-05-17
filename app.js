c73c8b69e2351f4ce19de6722c66e99d5754afbe
Paulo Pires <taralloqueiroiz@gmail.com>
2014-05-17 13:28:55 -0300

    added a list of archives

--- app.js
+++ app.js
@@ -6,6 +6,8 @@
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
+var multiparty = require('multiparty');
+var fs         = require('fs');
 
 // this will let us get the data from a POST
 app.use(bodyParser());
@@ -13,7 +15,6 @@ app.use(bodyParser());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'))
-app.use(express.multipart({ uploadDir: __dirname + '/uploads', limit: '50mb' }));
 
 var port = process.env.PORT || 8080;    // set our port
 
@@ -21,10 +22,31 @@ var port = process.env.PORT || 8080;    // set our port
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index')
+  fs.readdir(__dirname+'/uploads', function(err, files){
+    if (err) throw err;
+    res.render('index', {
+      files: files
+    });
+  })
 });
 
-// more routes for our API will happen here
+router.post('/upload', function(req, res) {
+  var form = new multiparty.Form({autoFiles: true, uploadDir: __dirname+'/uploads'});
+
+  form.parse(req, function(err, fields, files) {
+    console.log(files);
+    res.send(200)
+    // if (err) {
+    //   res.writeHead(400, {'content-type': 'text/plain'});
+    //   res.end("invalid request: " + err.message);
+    //   return;
+    // }
+    // res.writeHead(200, {'content-type': 'text/plain'});
+    // res.write('received fields:\n\n '+util.inspect(fields));
+    // res.write('\n\n');
+    // res.end('received files:\n\n '+util.inspect(files));
+  });
+})
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,6 +4,7 @@ $('#fileupload').fileupload({
   },
   progressall: function (e, data) {
     var progress = parseInt(data.loaded / data.total * 100, 10);
+    console.log(progress);
     $('.progress-bar').css('width', progress + '%');
   }
 });
