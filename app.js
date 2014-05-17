76c205eb00271df2ad0fe44b10b5f203fd5ce714
Paulo Pires <taralloqueiroiz@gmail.com>
2014-05-17 11:33:14 -0300

    added a multiparty

--- app.js
+++ app.js
@@ -13,14 +13,13 @@ app.use(bodyParser());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'))
+app.use(express.multipart({ uploadDir: __dirname + '/uploads', limit: '50mb' }));
 
 var port = process.env.PORT || 8080;    // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();        // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+var router = express.Router();
+
 router.get('/', function(req, res) {
   res.render('index')
 });
