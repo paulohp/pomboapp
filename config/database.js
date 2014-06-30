aa736aa7ac70f990d21f141aecb4d6d8ed27be58
Paulo Pires <taralloqueiroiz@gmail.com>
2014-06-30 13:52:27 -0300

    using database from openshift

--- config/database.js
+++ config/database.js
@@ -1,4 +1,10 @@
 // config/database.js
-module.exports = {
-  'url' : 'mongodb://localhost/pomboapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-};
+if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
+  module.exports = {
+    'url' : 'mongodb://admin:tbJ4WjhiFXug@$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/pombo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  };
+}else{
+  module.exports = {
+    'url' : 'mongodb://localhost/pomboapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  };
+}
