5f383c99cad38ab30e6303c8c247364753062c0e
Paulo Pires <taralloqueiroiz@gmail.com>
2014-07-01 13:19:44 -0300

    change url host

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
   module.exports = {
-    'url' : 'mongodb://admin:tbJ4WjhiFXug@$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/pombo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://admin:tbJ4WjhiFXug@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT' + /pombo' 
   };
 }else{
   module.exports = {
