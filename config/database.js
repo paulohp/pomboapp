3667baf3138de7c0beea6dacdbb55d372f130e85
Paulo Pires <taralloqueiroiz@gmail.com>
2014-07-04 12:01:00 -0300

    lol

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
 // config/database.js
-if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
-  module.exports = {
-    'url' : 'mongodb://admin:tbJ4WjhiFXug@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/pombo' 
-  };
-}else{
-  module.exports = {
-    'url' : 'mongodb://localhost/pomboapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-  };
-}
+// if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
+//
+// }else{
+//   module.exports = {
+//     'url' : 'mongodb://localhost/pomboapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+//   };
+// }
+
+
+module.exports = {
+  'url' : 'mongodb://admin:tbJ4WjhiFXug@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/pombo'
+};
