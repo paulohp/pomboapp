d0dabb94e5180f382a76ad8bcd2f0954bb0ef098
Paulo Pires <taralloqueiroiz@gmail.com>
2014-07-05 13:39:43 -0300

    new good home

--- models/file.js
+++ models/file.js
@@ -1,6 +1,4 @@
-
 var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our user model
 var userSchema = mongoose.Schema({
@@ -40,17 +38,6 @@ var userSchema = mongoose.Schema({
     }
 
 });
-
-// methods ======================
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
