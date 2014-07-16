e2e8a75e320032b14bd418508307b3a4e2d1180c
Paulo Pires <taralloqueiroiz@gmail.com>
2014-07-16 02:02:01 -0300

    added some dependencies

--- models/file.js
+++ models/file.js
@@ -1,6 +1,9 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
+var mongoose  = require('mongoose');
+var Schema    = mongoose.Schema;
+var ObjectId  = Schema.ObjectId;
+var Promise   = require('bluebird');
+var mbUtils   = require('mongoose-bluebird-utils');
+var _         = require('lodash');
 
 // define the schema for our user model
 var fileSchema = mongoose.Schema({
@@ -11,6 +14,17 @@ var fileSchema = mongoose.Schema({
     user                  :   { type: ObjectId, ref: 'User'},
 
 });
+
+fileSchema.methods.getUsedStorage = function(user) {
+  var aggregation = mongoose.model('File')
+    .aggregate({$match: {user: user._id}},
+               {$group: {_id: {}, total_storage: {$sum: '$size'}}});
+
+  return mbUtils.execP(aggregation).then(function(r) {
+    var f = _.first(r);
+    return f && f.total_storage;
+  });
+};
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('File', fileSchema);
