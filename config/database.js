891254edfa6a698bb81d4777ac708c8c296e3072
Paulo Pires <taralloqueiroiz@gmail.com>
2014-11-22 23:00:50 -0200

    added use and password to mgo connect

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 // config/database.js
 if (process.env.MONGODB_HOST) {
-  console.log(process.env.MONGODB_HOST)
   module.exports = {
-    'url' : process.env.MONGODB_URI
+    'url' : process.env.MONGO_URL,
+    'user': MONGODB_USERNAME,
+    'pass': MONGODB_PASSWORD
   };
 }else{
-  console.log("ERRRR")
   module.exports = {
     'url' : 'mongodb://localhost/pomboapp'
   };
