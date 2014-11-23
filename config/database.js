d284d5ca776533b9c244e9f744fe4b859d350a23
Paulo Pires <taralloqueiroiz@gmail.com>
2014-11-22 22:55:28 -0200

    logging database error

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 // config/database.js
-console.log(process.env)
 if (process.env.MONGODB_HOST) {
+  console.log(process.env.MONGODB_HOST)
   module.exports = {
     'url' : process.env.MONGODB_URI
   };
 }else{
+  console.log("ERRRR")
   module.exports = {
     'url' : 'mongodb://localhost/pomboapp'
   };
