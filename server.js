cdd04a5c3881042e1ca8da69631fb0de3aed5bc2
Paulo Pires <taralloqueiroiz@gmail.com>
2014-11-22 22:29:26 -0200

    port

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use(function (req, res, next) {
 require('./config/passport')(passport); // pass passport for configuration
 require('./app/routes.js')(app, express, passport, fs, Busboy, _, io); // load our routes and pass in our app and fully configured passport
 
-var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port =  process.env.PORT || 8080;
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 // START THE SERVER
