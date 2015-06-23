8379a6ada9a36c811be67691aa41f456fc77ca46
Paulo Pires <taralloqueiroiz@gmail.com>
2015-06-23 08:59:59 -0300

    removin unused oauth rout

--- app/routes.js
+++ app/routes.js
@@ -74,34 +74,6 @@ module.exports = function(app, express, passport, fs, Busboy, _, io){
     failureFlash : true
   }));
 
-  router.post('/oauth',function(req, res) {
-    var email = req.body.email || '';
-    var password = req.body.password || '';
-
-    if (email == '' || password == '') {
-        return res.send(401);
-    }
-
-    User.findOne({'local.email': email}, function (err, user) {
-      if (err) {
-          console.log(err);
-          return res.send(401);
-      }
-
-      user.comparePassword(password, function(isMatch) {
-        if (!isMatch) {
-            console.log("Attempt failed to login with " + user.username);
-            return res.send(401);
-        }
-
-        var token = jwt.sign(user, secret.secretToken, { expiresInMinutes: 60 });
-
-        return res.json({token:token});
-      });
-
-    });
-  });
-
   router.get('/signup', function(req, res) {
     res.render('user/signup', { message: req.flash('signupMessage') });
   });
